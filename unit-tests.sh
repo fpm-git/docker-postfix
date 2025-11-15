8ad6de0d83bcba4d8d85e5ec33e5c2b9b11080c9
Bojan Čekrlić <bojan@cekrlic.com>
2025-11-15 17:08:41 +0100

    Upd: Add support for running tests with podman.

--- unit-tests.sh
+++ unit-tests.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
+set -u
+set -x
 cd unit-tests
 
-DOCKER_COMPOSE="docker-compose"
-if docker --help | grep -q -F 'compose*'; then
-    DOCKER_COMPOSE="docker compose"
+if command -v docker >/dev/null 2>&1; then
+    DOCKER_COMPOSE="docker-compose"
+    if docker --help | grep -q -F 'compose*'; then
+        DOCKER_COMPOSE="docker compose"
+    fi
+elif command -v podman >/dev/null 2>&1; then
+    DOCKER_COMPOSE="podman compose"
+else
+    echo "Neither `docker` or `podman` installed. Cannot execute tests."
+    exit 1
 fi
 
 $DOCKER_COMPOSE up --build --abort-on-container-exit --exit-code-from tests
